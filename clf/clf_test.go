f06e9b93a1eea940ac0bfdcbb79636c95a84c12f
Jaime Tenorio <jaimeteb@gmail.com>
2021-02-05 18:58:23 -0600

    Test refactored clf

--- clf/clf_test.go
+++ clf/clf_test.go
@@ -1,24 +1,28 @@
 package clf
 
-// func TestClf1(t *testing.T) {
-// 	path := "../examples/00_test/"
-// 	classif := Create(&path)
-// 	pred1, _ := classif.Predict("on")
-// 	if pred1 != "turn_on" {
-// 		t.Errorf("pred is incorrect, got: %v, want: %v.", pred1, "turn_on")
-// 	}
-// 	pred2, _ := classif.Predict("foo")
-// 	if pred2 != "" {
-// 		t.Errorf("pred is incorrect, got: %v, want: %v.", pred2, "")
-// 	}
-// }
+import (
+	"testing"
+)
 
-// func TestClf2(t *testing.T) {
-// 	path := "../examples/404/"
-// 	defer func() {
-// 		if r := recover(); r == nil {
-// 			t.Errorf("No panic")
-// 		}
-// 	}()
-// 	Create(&path)
-// }
+func TestClf1(t *testing.T) {
+	path := "../examples/00_test/"
+	classif := Create(&path)
+	pred1, _ := classif.Model.Predict("on", classif.Pipeline)
+	if pred1 != "turn_on" {
+		t.Errorf("pred is incorrect, got: %v, want: %v.", pred1, "turn_on")
+	}
+	pred2, _ := classif.Model.Predict("foo", classif.Pipeline)
+	if pred2 != "" {
+		t.Errorf("pred is incorrect, got: %v, want: %v.", pred2, "")
+	}
+}
+
+func TestClf2(t *testing.T) {
+	path := "../examples/404/"
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("No panic")
+		}
+	}()
+	Create(&path)
+}
