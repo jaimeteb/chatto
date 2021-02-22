324c2b241f867f6d16acbfba6b622b9e767a8a33
Ryan Currah <ryan@currah.ca>
2021-02-22 07:35:36 -0500

    improve goreleaser config and add version flags to commands

--- cmd/chatto-cli/main.go
+++ cmd/chatto-cli/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/jaimeteb/chatto/bot"
 	"github.com/jaimeteb/chatto/internal/logger"
+	"github.com/jaimeteb/chatto/internal/version"
 )
 
 func main() {
 	url := flag.String("url", "http://localhost", "Specify url to use.")
 	port := flag.Int("port", 4770, "Specify port to use.")
 	debug := flag.Bool("debug", false, "Enable debug logging.")
+	vers := flag.Bool("version", false, "Display version.")
 	flag.Parse()
+
+	if *vers {
+		fmt.Println(version.Build())
+		return
+	}
 
 	logger.SetLogger(*debug)
 
--- cmd/chatto/main.go
+++ cmd/chatto/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
+
+	"github.com/jaimeteb/chatto/internal/version"
 
 	"github.com/jaimeteb/chatto/bot"
 	"github.com/jaimeteb/chatto/internal/logger"
@@ -11,7 +14,13 @@ func main() {
 	port := flag.Int("port", 4770, "Specify port to use.")
 	path := flag.String("path", ".", "Path to YAML files.")
 	debug := flag.Bool("debug", false, "Enable debug logging.")
+	vers := flag.Bool("version", false, "Display version.")
 	flag.Parse()
+
+	if *vers {
+		fmt.Println(version.Build())
+		return
+	}
 
 	logger.SetLogger(*debug)
 
