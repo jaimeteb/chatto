f3f43646a2e6fee50158a35f57778e878a441ea0
Ryan Currah <ryan@currah.ca>
2021-03-07 21:16:19 -0500

    use consistent name for bot (#34)

--- bot/server.go
+++ bot/server.go
@@ -9,12 +9,12 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Server runs botto bot
+// Server runs chatto bot
 type Server struct {
 	bot *bot.Bot
 }
 
-// NewServer for running botto bot
+// NewServer for running chatto bot
 func NewServer(path string, port int) *Server {
 	botConfig, err := bot.LoadConfig(path, port)
 	if err != nil {
@@ -56,7 +56,7 @@ func (s *Server) SlackHandler(w http.ResponseWriter, r *http.Request) {
 	s.bot.ChannelHandler(w, r, s.bot.Channels.Slack)
 }
 
-// Run botto bot server
+// Run chatto bot server
 func (s *Server) Run() {
 	s.bot.Run()
 }
