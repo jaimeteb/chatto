af8ac8e2523e03ecfe74006cc8b266df47523549
Jaime Tenorio <jaimeteb@gmail.com>
2023-01-28 23:42:13 +0100

    Add clf save and load tests

--- internal/clf/naivebayes/naivebayes_test.go
+++ internal/clf/naivebayes/naivebayes_test.go
@@ -1,9 +1,12 @@
 package naivebayes_test
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/jaimeteb/chatto/internal/clf"
+	"github.com/jaimeteb/chatto/internal/clf/naivebayes"
 	"github.com/jaimeteb/chatto/internal/testutils"
 )
 
@@ -25,6 +28,32 @@ func TestClfPredictions(t *testing.T) {
 	if pred2 != "" {
 		t.Errorf("pred is incorrect, got: %v, want: %v.", pred2, "")
 	}
+	t.Cleanup(func() {
+		testutils.RemoveFiles("gob")
+	})
+}
+
+func TestSaveAndLoad(t *testing.T) {
+	classifConfig, err := clf.LoadConfig("../../"+testutils.Examples00TestPath, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	classifOld := naivebayes.NewClassifier(map[string]interface{}{"tfidf": true})
+	classifOld.Learn(classifConfig.Classification, &classifConfig.Pipeline)
+	if err = classifOld.Save("./"); err != nil {
+		t.Error(err)
+	}
+
+	classifNew, err := naivebayes.Load("./")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(classifOld, classifNew) {
+		t.Errorf("Classifier saved %v, Classifier loaded %v", spew.Sprint(classifOld), spew.Sprint(classifNew))
+	}
+
 	t.Cleanup(func() {
 		testutils.RemoveFiles("gob")
 	})
